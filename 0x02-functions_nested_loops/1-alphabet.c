d6ccbb5dcdd206dc64b4dfe4537e11ed7a9eb3cb
Ntethelelo063 <ntethelelobhekiswayo@gmail.com>
2023-06-20 14:47:08 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,8 +4,13 @@
 */
 void print_alphabet(void)
 {
-char letter;
-for (letter = 'a'; letter <= 'z'; letter++)
-_putchar(letter);
-_putchar('\n');
+	
+    char letter;
+
+    for (c  = 'a'; c <= 'z'; c++)
+    {
+    _putchar(c);
+    }
+    _putchar('\n');
+
 }
